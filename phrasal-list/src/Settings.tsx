2d4e51c3984a94c9df9a1d8dd3a5364844750467
Manuel <manuelvgc2233@gmail.com>
2025-04-07 11:35:44 -0600

    Config finished

--- phrasal-list/src/Settings.tsx
+++ phrasal-list/src/Settings.tsx
@@ -1,14 +1,36 @@
-import { Mail, User } from "lucide-react";
+import { AlertCircle, Mail, User } from "lucide-react";
 import { Button } from "./components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./components/ui/tabs";
 import { Label } from "./components/ui/label";
 import { Input } from "./components/ui/input";
+import { useState } from "react";
+import { toast } from "sonner";
+import { Alert, AlertDescription, AlertTitle } from "./components/ui/alert";
 
 
 
 function SettingsStudents() {
+    const [passData, setPassData] = useState({
+        currentPass: "",
+        newPass: "",
+        confirmPass: ""
+    })
     
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        if( passData.newPass !== passData.confirmPass ){
+            toast.error("Error", {
+                description: "Las contaseñas no coinciden",                
+            })
+        }else{
+            toast("Listo!", {
+                description: "Contraseñas cambiadas correctamente"
+            })
+        }
+
+    }
 
     return (
         <>
@@ -76,28 +98,39 @@ function SettingsStudents() {
                                 <CardDescription>Actualiza tu contraseña para mantener tu cuenta segura.</CardDescription>
                             </CardHeader>
                             <CardContent>
-                                <form onSubmit={() => "Función de enviar"}>
+                                <form onSubmit={handleSubmit}>
                                     <div className="grid items-start grid-cols-1 md:grid-cols-[1fr] gap-4 w-full">
                                         <div className="flex flex-col gap-2">
                                             <Label htmlFor="current-pass" className="text-md">
                                                 Contraseña Actual
                                             </Label>
-                                            <Input id="current-pass" type="password"/>
+                                            <Input 
+                                                onChange={ (e) =>setPassData( prev => ({...prev, currentPass: e.target.value}) )}
+                                                id="current-pass" 
+                                                type="password"/>
                                         </div>
                                         <div className="flex flex-col gap-2">
                                             <Label htmlFor="current-pass" className="text-md">
                                                 Nueva Contraseña
                                             </Label>
-                                            <Input id="new-pass" type="password"/>
+                                            <Input 
+                                                onChange={ e => setPassData( prev => ({...prev, newPass: e.target.value}))}
+                                                id="new-pass" 
+                                                type="password"/>
                                         </div>
                                         <div className="flex flex-col gap-2">
                                             <Label htmlFor="current-pass" className="text-md">
                                                 Confirmar Nueva Contraseña
                                             </Label>
-                                            <Input id="confirm-new-pass" type="password"/>
+                                            <Input
+                                                onChange={ e => setPassData( prev => ({...prev, confirmPass: e.target.value}))}
+                                                id="confirm-new-pass" 
+                                                type="password"/>
                                         </div>
                                     </div>
-                                    <Button className="mt-6 w-full bg-amber-500 hover:bg-amber-600 hover:cursor-pointer" type="submit">
+                                    <Button 
+                                        className="mt-6 w-full bg-amber-500 hover:bg-amber-600 hover:cursor-pointer" 
+                                        type="submit">
                                         Cambiar Contraseña
                                     </Button>
                                 </form>
@@ -106,6 +139,34 @@ function SettingsStudents() {
                         </Card>
                     </TabsContent>
                 </Tabs>
+            </div>
+            <div className="flex justify-center gap-4 pl-6 pr-6 w-full mb-6">
+                <Card className="w-full">
+                    <CardHeader>
+                        <CardTitle className="text-2xl text-red-400 font-bold">
+                            Zona de Peligro
+                        </CardTitle>
+                        <CardDescription>
+                            Acciones irreversibles para tu cuenta
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <Alert variant={"destructive"} className="border-red-400">
+                        <AlertCircle className="h-4 w-4" />
+                        <AlertTitle>Avertencia</AlertTitle>
+                        <AlertDescription>
+                            Las siguientes acciones son permanentes y no se pueden deshacer.
+                        </AlertDescription>
+                        </Alert>
+                    </CardContent>
+                    <CardFooter>
+                        <Button 
+                            className="w-full border border-red-400 bg-transparent text-red-400
+                            hover:bg-red-50 hover:text-black hover:cursor-pointer">
+                            Eliminar cuenta
+                        </Button>
+                    </CardFooter>
+                </Card>
             </div>
         </>
     )
